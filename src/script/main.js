94fd894a203c4042870f4541590b757e662c8286
Li <eseredich8@gmail.com>
2025-05-26 21:57:06 +0300

    feat: add stop and play

--- src/script/main.js
+++ src/script/main.js
@@ -4,7 +4,6 @@ class myGame {
     items = [];
     complete = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0];
 
-
     clickNumber = 0;
     clickNumberId = 0;
     zeroId = 13;
@@ -13,8 +12,12 @@ class myGame {
     time = 0;
     timerId = 0;
     
-    shakeArr = (arr)=>{
-        this.items = arr.toSorted(() => Math.random() - 0.5);
+    createItems = ()=>{
+        const startItems = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+        this.items = startItems.toSorted(() => Math.random() - 0.5);
+        this.items.push(14);
+        this.items.push(15);
+        this.items.push(0);
     }
 
     findIndex = (clickNumber)=>{
@@ -46,13 +49,24 @@ class myGame {
     };
 
     isComplete = ()=>{
-      return JSON.stringify(this.items) == JSON.stringify(this.complete);
+        return JSON.stringify(this.items) == JSON.stringify(this.complete);
     };
+
+    newGame = ()=>{
+        this.clickNumber = 0;
+        this.clickNumberId = 0;
+        this.zeroId = 13;
+
+        this.steps = 0;
+        this.time = 0;
+        this.timerId = 0;
+        this.createItems();
+    }
 }
 
 
 gameData = new myGame();
-gameData.shakeArr(gameData.complete);
+gameData.createItems();
 gameData.timerId = setInterval(()=> this.time++, 1000);
 
 createApp({
@@ -63,16 +77,19 @@ createApp({
             time: gameData.time,
             timerId: false,
             complete: false,
+            notStop: true,
+            inputValue: '',
+            size: 3
         }
     },
     methods:{
-        moveBlock(num){
+        moveBlock(num, ind){
             if(num == 0){
                 return;
             }
 
             if(!this.timerId){
-                this.timerId = setInterval(()=> this.time++, 1000);
+                this.timerId = setInterval(()=>  this.time++, 1000);
             }
             
             gameData.findIndex(num);
@@ -83,28 +100,45 @@ createApp({
                 this.complete = gameData.isComplete();
 
             if(this.complete){
-              clearInterval(this.timerId);
+                clearInterval(this.timerId);
             }
           }
         },
-
-        // pause(){
-        //   this.isStop = true;
-        //   clearInterval(this.timerId);
-        //   newGame.clearTimer();
-        // },
-
-        // restart(){
-        //   clearInterval(this.timerId);
-        //   newGame.clearTimer();
-        //   this.steps = 0;
-        //   this.timer = 0;
-        //   this.isFinish = false;
-        //   this.isStop = false;
-        //   this.my_state = newGame.shakeArr().flat();
-        //   this.isStart = true;
-        //   this.timerId = setInterval(()=> this.timer++, 1000);
-        // },
+        getTime(){
+            const sec = this.time%60+'';
+            const min = Math.floor(this.time/60)+'';
+            return `${min.padStart(2, '0')}:${sec.padStart(2, '0')}`
+        },
+        pause(){
+            this.notStop = false;
+            clearInterval(this.timerId);
+            gameData.timerId = 0;
+        },
+        play(){
+            this.notStop = true;
+            this.timerId = setInterval(()=> this.time++, 1000)
+        },
+        restart(){
+            this.notStop = false;
+            clearInterval(this.timerId);
+            newGame();
+            // newGame.clearTimer();
+            this.timerId = setInterval(()=> this.time++, 1000);
+        },
+    },
+     // должны возвращать что-то и в html вызываем без ()
+    //  если результат зависит от какого-то свойства 
+    computed:{ 
+        doubleCount(){
+            console.log('double')
+            return this.items.length * 2
+        }
+    },
+    // следить за изменением какого-то свойства 
+    watch:{
+        inputValue(value){
+            console.log(value)
+        }
     }
 }).mount('#app')
 
